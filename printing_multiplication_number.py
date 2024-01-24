6b662688cffd84a411d7ca468a2198bc9b19c441
veronbungag <rveron.bungag@gmail.com>
2024-01-24 23:23:02 +0800

    organize multiplication table finally done yeyy

--- printing_multiplication_number.py
+++ printing_multiplication_number.py
@@ -1,7 +1,9 @@
 #for loop range 1-11 number
 for i in range(1, 11):
     for j in range(1, 11):
-    #calculate two numbers then store in product
+        #calculate two numbers then store in product
         product = i * j
         #print product with 5 width spacing
         print(f"{product:5}", end=" ")
+    #print in new line
+    print("\n")
