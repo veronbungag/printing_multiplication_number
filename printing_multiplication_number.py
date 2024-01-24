472ebd72f9c7d4c525e62e6427065c789babef01
veronbungag <rveron.bungag@gmail.com>
2024-01-24 23:02:30 +0800

    calculating the product

--- printing_multiplication_number.py
+++ printing_multiplication_number.py
@@ -1,5 +1,6 @@
 #for loop range 1-11 number
 for i in range(1, 11):
     for j in range(1, 11):
-#calculate two numbers then store in product
+    #calculate two numbers then store in product
+    product = i * j
 #print product with 5 width spacing
